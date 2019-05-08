cff6db25ccc920374f25978e40516441da5ab79b
songzongquan <songzongquan@bonc.com.cn>
2019-05-08 17:33:59 +0800

    添加main.py文件，并测试分包机制

--- flowControler/flowControler.py
+++ flowControler/flowControler.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 #encoding:utf-8
 
+from common.emailClient import EmailClient 
+
+class FlowControler:
+    '''主控类'''
+    def main(self):
+        print('hello,world')
+
+if __name__ == '__main__':
+    f = FlowControler()
+    f.main()
 
 
