9cd4321819a323ceae28333442013c254f0bdadf
AegeanSeaBlue <1757793899@qq.com>
2019-05-09 15:32:11 +0800

    add excel解析器

--- common/excelTemplateParser.py
+++ common/excelTemplateParser.py
@@ -1,6 +1,20 @@
-# coding:utf-8
+# encoding:utf-8
 import json
+import os
 class ExcelTempleteParser():
     def parse(templateName):
-        d = json.load(open('/tmp/result.txt','r'))
-        print(d)
+        a = os.getcwd()
+        print(a)
+        b = os.path.dirname(a)
+        path = os.path.join(b+'/flowControler/data/'+'form_邮箱注册.json')
+        print(path)
+        with open(path) as form:
+            aa = json.load(form, encoding='utf-8')
+            print(aa)
+e = ExcelTempleteParser()
+e.parse()
+
+
+
+
+
