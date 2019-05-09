a5818cd324ab26a0ea87c8962b7e69c834b8c0c9
AegeanSeaBlue <1757793899@qq.com>
2019-05-09 16:03:12 +0800

    add

--- common/excelTemplateParser.py
+++ common/excelTemplateParser.py
@@ -2,17 +2,20 @@
 import json
 import os
 class ExcelTempleteParser():
-    def parse(templateName):
-        a = os.getcwd()
-        print(a)
-        b = os.path.dirname(a)
-        path = os.path.join(b+'/flowControler/data/'+'form_邮箱注册.json')
+    def parse(self,templateName):
+        base = os.getcwd()
+        print(base)
+        b = os.path.dirname(base)
+        path = os.path.join(b+'/flowControler/data/'+templateName)
         print(path)
         with open(path) as form:
             aa = json.load(form, encoding='utf-8')
             print(aa)
-e = ExcelTempleteParser()
-e.parse()
+# e = ExcelTempleteParser()
+# e.parse()
+if __name__=='__main__':
+    e = ExcelTempleteParser()
+    e.parse('form_邮箱注册.json')
 
 
 
