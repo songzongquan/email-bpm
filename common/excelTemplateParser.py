b2bba6e9491926156b49a72301400798bab9cbe8
AegeanSeaBlue <1757793899@qq.com>
2019-05-10 09:47:53 +0800

    add

--- common/excelTemplateParser.py
+++ common/excelTemplateParser.py
@@ -2,20 +2,27 @@
 import json
 import os
 class ExcelTempleteParser():
+    '''excel模板定义解析器，读取指定的json文件，得到excel模板定义信息，返回一个字典数据结构;'''
     def parse(self,templateName):
-        base = os.getcwd()
+        '''此程序共4个json文件，分别对应4种excel模板文件。
+            此方法读取templateName对应的json文件，将json文件反序列化，返回字典。
+            '''
+        base = os.path.dirname(__file__)  #获得__file__所在的路径，是当前方法的绝对路径。
         print(base)
-        b = os.path.dirname(base)
-        path = os.path.join(b+'/flowControler/data/'+templateName)
+        base1 = os.path.dirname(base)
+        #print(base1)
+        path = os.path.join(base1+'/flowControler/data/'+templateName)  #获得路径加json文件名
         print(path)
-        with open(path) as form:
-            aa = json.load(form, encoding='utf-8')
-            print(aa)
+        with open(path,encoding='utf-8') as form_info:
+            form = json.load(form_info)
+            print(form)
+        return form
 # e = ExcelTempleteParser()
 # e.parse()
 if __name__=='__main__':
     e = ExcelTempleteParser()
     e.parse('form_邮箱注册.json')
+    e.parse('form_域名反向代理申请.json')
 
 
 
