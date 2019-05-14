61c8abb551befbb64900a699d144b142edb0359e
AegeanSeaBlue <1757793899@qq.com>
2019-05-14 15:04:47 +0800

    add excel读写器

--- common/excelTemplateParser.py
+++ common/excelTemplateParser.py
@@ -8,21 +8,21 @@ class ExcelTempleteParser():
             此方法读取templateName对应的json文件，将json文件反序列化，返回字典。
             '''
         base = os.path.dirname(__file__)  #获得__file__所在的路径，是当前方法的绝对路径。
-        print(base)
+        # print(base)
         base1 = os.path.dirname(base)
-        #print(base1)
+        # print(base1)
         path = os.path.join(base1+'/flowControler/data/'+templateName)  #获得路径加json文件名
-        print(path)
+        # print(path)
         with open(path,encoding='utf-8') as form_info:
-            form = json.load(form_info)
-            print(form)
-        return form
+            templateDef = json.load(form_info)
+            print(templateDef)
+            return templateDef
 # e = ExcelTempleteParser()
 # e.parse()
 if __name__=='__main__':
     e = ExcelTempleteParser()
     e.parse('form_邮箱注册.json')
-    e.parse('form_域名反向代理申请.json')
+    # e.parse('form_域名反向代理申请.json')
 
 
 
