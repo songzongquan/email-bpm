90ad64e3719b70ffaa5a31cc13e2551f4bca937a
shanghualan <shanghualan@bonc.com.cn>
2019-05-14 16:21:17 +0800

    修改流程定义解析器，修改获得json文件的路径

--- flowControler/flowDefineParser.py
+++ flowControler/flowDefineParser.py
@@ -8,13 +8,14 @@ class FlowDefineParser:
         '''此程序共4个json文件，分别对应4种流程。
            本方法读取filename对应的json文件，将json文件反序列化，返回字典。
         '''
-        path1 = os.path.dirname(__file__)           #返回的是__file__所在的路径，是当前方法的绝对路径。
+        path1 = (os.path.abspath(os.path.dirname(os.path.dirname(__file__))))           #返回的是__file__所在的路径的上一级路径，是当前方法的绝对路径的上一级路径。
         # print(path1)
         path2 = os.path.join(path1+"/data/"+filename)       #返回路径加文件名
         # print(path2)
         with open(path2,encoding="utf-8") as flow_info:
              flow = json.load(flow_info)
-             print(flow)
+             # print(flow)
+             return flow
 
 
 
