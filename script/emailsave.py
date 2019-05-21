c983c7ed07f041c924875691570ec4ff86b768fc
lixiaofan <lixiaofan@bonc.com.cn>
2019-05-21 14:23:49 +0800

    add

--- script/emailsave.py
+++ script/emailsave.py
@@ -1,1 +1,27 @@
-print('{"state":"success","msg":"执行成功"}')
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+import json
+
+def emailsave(name,mail):
+    path=os.path.abspath(os.path.join(os.getcwd(), ".."))+'\\data\\emailInfo.json'
+    # print(path)
+    #打开json文件，读取里面的内容
+    with open(path, encoding='utf-8') as form_info:
+        list = json.load(form_info)
+        # print(list)
+    #再list的最后一行，添加字典
+    dict={}
+    dict["姓名"]=name
+    dict["邮箱"]=mail
+    list.append(dict)
+    # print(list)
+    #将最新的list写入到json文件中
+    with open(path,'w',encoding='utf-8') as f:
+        f.write(json.dumps(list,ensure_ascii=False))
+
+
+
+
+if __name__ == '__main__':
+    emailsave("商华蓝","shanghualan@bonc.com.cn")
--- script/tongzhi.py
+++ script/tongzhi.py
@@ -1,1 +1,13 @@
-print('{"state":"success","msg":"发送通知邮件"}')
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+from common.emailClient import EmailClient
+
+def tongzhi(mailname,password,imaphost,imapport,smtphost,smtpport,toaddrs, subject, content):
+
+    a=EmailClient(mailname,password,imaphost,imapport,smtphost,smtpport)
+    a.sendMail(toaddrs, subject,content,'', '')
+
+
+if __name__ == '__main__':
+    tongzhi('lixiaofan@bonc.com.cn', 'Lixiaofan123', 'mail.bonc.com.cn', '993', 'mail.bonc.com.cn', '465','lixiaofan@bonc.com.cn','邮箱注册已经完成','邮箱注册已经完成')
