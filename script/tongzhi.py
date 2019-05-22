ed181ddcb770c7591f9501a356e05d1db8e8dfcd
songzongquan <songzongquan@bonc.com.cn>
2019-05-22 15:11:32 +0800

    添加了sys.path，以便 找到 common模块

--- script/tongzhi.py
+++ script/tongzhi.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 #encoding:utf-8
+import os,sys
 
+p = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+print(p)
+sys.path.append(p)
+
+print(sys.path)
 from common.emailClient import EmailClient
 from common.config import *
-
-
 
 
 def tongzhi(toaddrs, subject, content):
