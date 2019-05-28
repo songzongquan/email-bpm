66de81a1563a432551cab728e278938c1d0ee4f1
wff <user@user-PC>
2019-05-28 10:34:20 +0800

    tijiao

--- executeAgent/executeAgent.py
+++ executeAgent/executeAgent.py
@@ -35,13 +35,13 @@ class ShellExecuteAgent():
                 filename=os.path.join(downloadpath+attachmentname)# 精确到附件名称的路径
                 excelparse = ExcelReadWriter(filename)# 实例化表格解析函数
                 command = excelparse.read("script").split(" ")#从附件的sheet2中获取执行什么脚本，需要什么参数
-                script=['bash']
-                script.extend(command)#列表拼接，例['bash','脚本名','参数1’,'参数2',……]
-                executescript = " ".join(script)#执行shell脚本需要将上方的列表准换成字符串
-                executescript1 = executescript.split(' ') 
-                if ".sh" in executescript:
-                    status = subprocess.Popen(executescript,stdout=subprocess.PIPE,shell=True,cwd=os.getcwd()+"/script/").communicate()
-                    #获取shell脚本传回来的参数。通过subprocess.Popen()方法调用命令后执行的结果，stdout表示程序的标准输出；shell=True表示指定的命令将通过shell执行，cwd表示子进程的工作目录，即脚本所在目录，communicate返回标准输出。
+
+                if "revers_proxy.sh" in command:
+                    interpreter1 = ['bash']
+                    interpreter1.extend(command)#列表拼接，例['bash','脚本名','参数1’,'参数2',……]
+                    executescript1 = " ".join(interpreter1)  # 执行shell脚本需要将上方的列表准换成字符串
+                    path = os.path.join(os.path.dirname(__file__),'script')
+                    status = subprocess.Popen(executescript1,stdout=subprocess.PIPE,shell=True,cwd=path).communicate() #获取shell脚本传回来的参数。通过subprocess.Popen()方法调用命令后执行的结果，stdout表示程序的标准输出；shell=True表示指定的命令将通过shell执行，cwd表示子进程的工作目录，即脚本所在目录，communicate返回标准输出。
                     status1=str(status[0])
                     status2=status1.split('\\n')
                     excelparse.write("result", status2[1])#将脚本返回来的结果写入附件中
@@ -51,14 +51,17 @@ class ShellExecuteAgent():
                     excelparse.write("executionTime", executetime)
                     emailclient.sendMail("cloudiip_ops@bonc.com.cn","域名反向代理执行结果","域名反向代理工作已完成",attachmentname,downloadpath)
                     # mailclient.removeMessage(getmail)  # 删除邮件
-                elif ".py" in executescript:
-                    status = subprocess.Popen(executescript1,stdout=subprocess.PIPE,cwd=os.getcwd() + "/script/").communicate()
+                elif "host_auth.py" in command:
+                    interpreter2 = ['python']
+                    interpreter2.extend(command)
+                    path = os.path.join(os.path.dirname(__file__),'script')
+                    status = subprocess.Popen(interpreter2,stdout=subprocess.PIPE,cwd=path).communicate()
                     status1=str(status[0])
                     status2=status1.split('\\n')
-                    excelparse.write("执行结果", status2[1])
-                    excelparse.write("执行者", "auto")
+                    excelparse.write("result", status2[1])
+                    excelparse.write("executor", "auto")
                     executetime = time.strftime('%Y.%m.%d', time.localtime(time.time()))
-                    excelparse.write("执行时间", executetime)
+                    excelparse.write("executionTime", executetime)
                     emailclient.sendMail("cloudiip_ops@bonc.com.cn","DNS解析执行结果","DNS解析工作已完成",attachmentname,downloadpath)
                     #mailclient.removeMessage(getmail)  # 删除邮件
             print("5 minutes") 
